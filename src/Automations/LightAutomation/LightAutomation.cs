434a2b68b4746597713dedd878241ce560581c88
x00Pavel <pavel.yadlouski@gmail.com>
2023-09-03 19:07:58 +0200

    Add base automation class

--- src/Automations/LightAutomation/LightAutomation.cs
+++ src/Automations/LightAutomation/LightAutomation.cs
@@ -1,56 +1,22 @@
-﻿using System.Reactive.Linq;
+using System.Reactive.Linq;
 using Events;
 using Microsoft.Extensions.Logging;
 using NetDaemon.HassModel;
 using NetDaemon.HassModel.Entities;
-using NetEntityAutomation.FSM.LightFsm;
 
 namespace NetEntityAutomation.Automations.LightAutomation;
 
-public record LightAutomationConfiguration(string Name, IBinarySensorEntityCore Sensor, string SwitchId,
-    FsmConfig<OnOffFsmState> FsmConfig)
+public abstract class LightAutomation<TFsmState>: BaseAutomation<TFsmState> where TFsmState : struct, Enum
 {
-    public string? Name { get; init; } = Name;
-    public IBinarySensorEntityCore MotionSensors { get; init; } = Sensor;
-    public string Switch { get; init; } = SwitchId;
-    public FsmConfig<OnOffFsmState> FsmConfig { get; set; } = FsmConfig;
-}
+    protected LightAutomation(ILogger logger, LightAutomationConfiguration<TFsmState> config, IHaContext haContext) : base(logger, config, haContext)
+    {
+    }
 
-/// <summary>
-/// This automation expects that provided switch has 'on' and 'off' commands.
-/// </summary>
-public class LightAutomation
-{
-    private IHaContext _haContext;
-    private readonly ILogger _logger;
-    private readonly LightAutomationConfiguration _config;
-    private MotionSwitchLightFsm _fsm;
+    protected IObservable<StateChange> MotionSensorEvent =>
+        HaContext.StateChanges().Where(e => e.New?.EntityId == Config.MotionSensors.EntityId);
 
-    private IObservable<StateChange> MotionSensorEvent =>
-        _haContext.StateChanges().Where(e => e.New?.EntityId == _config.MotionSensors.EntityId);
-
-    private IObservable<ZhaEventData> SwitchEvent =>
-        _haContext.Events.Filter<ZhaEventData>("zha_event")
-            .Where(e => e.Data?.DeviceIeee == _config.SwitchId)
+    protected IObservable<ZhaEventData> SwitchEvent =>
+        HaContext.Events.Filter<ZhaEventData>("zha_event")
+            .Where(e => e.Data?.DeviceIeee == Config.SwitchId)
             .Select(e => e.Data!);
-
-    public LightAutomation(IHaContext ha, LightAutomationConfiguration config, ILogger logger)
-    {
-        _haContext = ha;
-        _logger = logger;
-        _config = config;
-
-        _fsm = new MotionSwitchLightFsm(logger, _config.FsmConfig);
-        InitFsmTransitions();
-        logger.LogInformation("LightAutomation initialised");
-    }
-
-    private void InitFsmTransitions()
-    {
-        _logger.LogInformation("Initialising FSM for {RoomName}", _config.Name);
-        MotionSensorEvent.Where(e => e.New?.State == "on").Subscribe(_ => _fsm.MotionOn());
-        MotionSensorEvent.Where(e => e.New?.State == "off").Subscribe(_ => _fsm.MotionOff());
-        SwitchEvent.Where(e => e.Command == "on").Subscribe(_ => _fsm.SwitchOn());
-        SwitchEvent.Where(e => e.Command == "off").Subscribe(_ => _fsm.SwitchOff());
-    }
 }
---
+++ src/Automations/LightAutomation/OnOffLightAutomation.cs
@@ -0,0 +1,36 @@
+﻿using System.Reactive.Linq;
+using Events;
+using Microsoft.Extensions.Logging;
+using NetDaemon.HassModel;
+using NetDaemon.HassModel.Entities;
+using NetEntityAutomation.FSM.LightFsm;
+
+namespace NetEntityAutomation.Automations.LightAutomation;
+
+/// <summary>
+/// This automation expects that provided switch has 'on' and 'off' commands.
+/// </summary>
+public class OnOffLightAutomation: LightAutomation<OnOffFsmState>
+{   
+    private readonly MotionSwitchLightFsm _fsm;
+
+    public OnOffLightAutomation(IHaContext ha, LightAutomationConfiguration<OnOffFsmState> config, ILogger logger): base(logger, config, ha)
+    {
+        _fsm = new MotionSwitchLightFsm(logger, Config.FsmConfig)
+        {
+            TimerTrigger = OnOffFsmTrigger.TimeElapsed,
+            StoragePath = $"storage/{config.Name}_fsm.json"
+        };
+        InitFsmTransitions();
+        logger.LogInformation("LightAutomation initialised");
+    }
+
+    protected sealed override void InitFsmTransitions()
+    {
+        Logger.LogInformation("Initialising FSM for {RoomName}", Config.Name);
+        MotionSensorEvent.Where(e => e.New?.State == "on").Subscribe(_ => _fsm.MotionOn());
+        MotionSensorEvent.Where(e => e.New?.State == "off").Subscribe(_ => _fsm.MotionOff());
+        SwitchEvent.Where(e => e.Command == "on").Subscribe(_ => _fsm.SwitchOn());
+        SwitchEvent.Where(e => e.Command == "off").Subscribe(_ => _fsm.SwitchOff());
+    }
+}
