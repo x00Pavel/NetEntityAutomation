111acc1e1602c39cbf0ddaf46f7d667488623e99
x00Pavel <pavel.yadlouski@veeam.com>
2024-02-11 11:19:05 +0100

    Add generic class for Conditions

--- src/Room/Conditions/ICondition.cs
+++ src/Room/Conditions/ICondition.cs
@@ -9,3 +9,8 @@ public class DefaultCondition: ICondition
 {
     public bool IsTrue() => true;
 }
+
+public class Condition(Func<bool> condition) : ICondition
+{
+    public bool IsTrue() => condition();
+}
